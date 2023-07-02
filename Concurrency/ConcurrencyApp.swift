588f591b5e5529b3b021cb7ca23308e63e4b50ad
hide0101 <hideakikomori0101@gmail.com>
2023-07-02 14:58:50 +0900

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by hide0101 on 2023/07/02.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
